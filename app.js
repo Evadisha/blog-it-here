03e48b2f6d320db787f2e54e6dbdde64ee6dd600
Vedisha singh <54761281+Evadisha@users.noreply.github.com>
2020-12-23 19:24:13 +0530

    Initial commit.

---
+++ app.js
@@ -0,0 +1,96 @@
+//jshint esversion:6
+
+// Requiring the packages express, mongoose, ejs, body parser.
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+
+// The starting content for the blog for all the three pages.
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+// Use of the expres routing 
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+// Connection to mongoDB Atlas for the database.
+mongoose.connect("<The connection URL given by mongodb Atlas>",{useNewUrlParser:true, useUnifiedTopology:true});
+
+// The schema of post
+const postSchema = {
+  title: String,
+  content: String
+};
+
+// Making of a model Post for the post collection which follows the postSchema
+const Post = mongoose.model("post",postSchema);
+
+// To get the home route.
+app.get("/", function(req, res){
+  Post.find({}, function(err, posts){
+
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  })
+});
+
+// To get home page
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+// To get contact page 
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+// This route is to compose new posts.
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+//Here the new post is added and then the title and content is saved.
+app.post("/compose", function(req, res){
+  
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save(function(err,result){
+    if(err)
+      console.log("Unsuccessful attempt!");
+    else 
+      res.redirect("/");  
+  });
+});
+
+// To get the particular post using the obectId for finding the post in database
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+  
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+});
+
+// To listen either on 3000 or server port 
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started on port.");
+});
